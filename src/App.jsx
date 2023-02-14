dcbd436581ec43839bd655a114cd9a4ae5127995
carlosgarcia32 <69259477+carlosgarcia32@users.noreply.github.com>
2023-02-14 01:04:52 -0500

    Add theme-color meta tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
+import { useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { useSelector, useDispatch } from "react-redux"
 
-import { goTo } from "./store/features"
-
 import { MainLayout } from './layouts'
 
 import { Home } from './components'
+import { Helmet } from "react-helmet"
 
 function App() {
   const dispatch = useDispatch()
   const { defaultTab, availableTabs } = useSelector(state => state.tabManager)
+  const theme = useSelector(state => state.theme)
+  const [metaThemeColor, setMetaThemeColor] = useState(() => {
+    return theme.selected === "dark" ? "#121212" : "#ffffff"
+  })
+
+  useEffect(() => {
+    setMetaThemeColor((prev) => {
+      return theme.selected === "dark" ? "#121212" : "#ffffff"
+    })
+  }, [theme.selected])
   
   return (
     <div className="bg-gray-50 dark:bg-black">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content={metaThemeColor} />
+      </Helmet>
       <BrowserRouter>
         <Routes>
           <Route element={<MainLayout />}>
